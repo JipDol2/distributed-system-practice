b3f998c1965deac5d3bf4184ca6e344c1b7d2ce6
seongju <gch03915@naver.com>
2025-10-29 21:47:00 +0900

    first commit

---
+++ src/main/java/com/example/distributedsystempractice/DistributedSystemPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedsystempractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSystemPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/distributedsystempractice/DistributedSystemPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedsystempractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
